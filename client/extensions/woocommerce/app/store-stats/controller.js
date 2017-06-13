fd259b0ef5fa84f4e8c694811d714007c936b146
Robert Elliott <robert.elliott@automattic.com>
2017-06-13 10:36:03 +0200

    updated routes for product, categories and coupons to listeview component

--- client/extensions/woocommerce/app/store-stats/controller.js
+++ client/extensions/woocommerce/app/store-stats/controller.js
@@ -14,9 +14,10 @@ import AsyncLoad from 'components/async-load';
 import StatsPagePlaceholder from 'my-sites/stats/stats-page-placeholder';
 import { getQueryDate } from './utils';
 
+
 function isValidParameters( context ) {
 	const validParameters = {
-		type: [ 'orders' ],
+		type: [ 'orders', 'products', 'categories', 'coupons' ],
 		unit: [ 'day', 'week', 'month', 'year' ],
 	};
 	return Object.keys( validParameters )
@@ -34,14 +35,23 @@ export default function StatsController( context ) {
 		queryDate: getQueryDate( context ),
 		selectedDate: context.query.startDate || moment().format( 'YYYY-MM-DD' ),
 	};
-	renderWithReduxStore(
-		<AsyncLoad
+	const asyncComponent = ( props.type === 'orders' )
+		? <AsyncLoad
 			/* eslint-disable wpcalypso/jsx-classname-namespace */
 			placeholder={ <StatsPagePlaceholder className="woocommerce" /> }
 			/* eslint-enable wpcalypso/jsx-classname-namespace */
 			require="extensions/woocommerce/app/store-stats"
 			{ ...props }
-		/>,
+		/>
+		: <AsyncLoad
+			/* eslint-disable wpcalypso/jsx-classname-namespace */
+			placeholder={ <StatsPagePlaceholder className="woocommerce" /> }
+			/* eslint-enable wpcalypso/jsx-classname-namespace */
+			require="extensions/woocommerce/app/store-stats/listview"
+			{ ...props }
+		/>;
+	renderWithReduxStore(
+		asyncComponent,
 		document.getElementById( 'primary' ),
 		context.store
 	);
